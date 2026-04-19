31b993de43bbd7535faa67833b173b7bc67fb13d
joshmlindsay <joshmlindsay95@hotmail.com>
2026-04-19 00:07:10 -0500

    trying to fix scoring page

--- Shrine_ACU_Web_Application/Program.cs
+++ Shrine_ACU_Web_Application/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Kiota.Http.HttpClientLibrary;
 using Radzen;
 using Shrine_ACU_Web_Application.Components;
@@ -7,7 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(options =>
+    {
+        options.DetailedErrors = builder.Environment.IsDevelopment();
+    });
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.RequestMethod |
+                            HttpLoggingFields.RequestPath |
+                            HttpLoggingFields.RequestQuery |
+                            HttpLoggingFields.ResponseStatusCode |
+                            HttpLoggingFields.Duration;
+});
 
 builder.Services.AddRadzenComponents();
 
@@ -48,6 +61,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseHttpLogging();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
